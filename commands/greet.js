c353e86df23ed7962180493ab6721b7d26461a4d
Adarsh16-30 <adarsh.utkarsh09@gmail.com>
2025-10-25 01:00:54 +0530

    Create a simple greet user command

---
+++ commands/greet.js
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+export default (program) => {
+  program
+    .command("greet <name>") // <name> is a required argument
+    .description("Greet a user by name")
+    .option("-u, --uppercase", "Make greeting uppercase") // -u is an optional flag
+    .action((name, options) => {
+      let message = `Hello, ${name}!`;
+      if (options.uppercase) message = message.toUpperCase();
+      console.log(chalk.greenBright(message));
+    });
+};
