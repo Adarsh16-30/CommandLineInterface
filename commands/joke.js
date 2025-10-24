725ae9204e12187cf8112629d0d155e2301fadac
Adarsh16-30 <adarsh.utkarsh09@gmail.com>
2025-10-25 01:03:22 +0530

    Wire up a command to fetch programming jokes

---
+++ commands/joke.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+import ora from "ora";
+import axios from "axios";
+
+export default (program) => {
+  program
+    .command("joke")
+    .description("Get a random programming joke")
+    .action(async () => {
+      const spinner = ora("Fetching a funny joke...").start();
+      try {
+        // Simple, no-auth API for jokes
+        const res = await axios.get("https://official-joke-api.appspot.com/jokes/programming/random");
+        const joke = res.data[0]; // This API returns an array
+        spinner.succeed("Got one!");
+        console.log(chalk.cyanBright(`${joke.setup}\n${joke.punchline}`));
+      } catch {
+        spinner.fail("Couldn’t fetch a joke!");
+      }
+    });
+};
