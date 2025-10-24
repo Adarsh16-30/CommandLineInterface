9d64e7444394c1fca3c6213b462a17e805e0830c
Adarsh16-30 <adarsh.utkarsh09@gmail.com>
2025-10-25 01:00:07 +0530

    Set up a simple local web server command

---
+++ commands/serve.js
@@ -0,0 +1,20 @@
+import express from "express";
+import chalk from "chalk";
+import open from "open";
+
+export default (program) => {
+  program
+    .command("serve [port]")
+    .description("Run a simple local dev server (default: 3000)")
+    .action((port = 3000) => {
+      const app = express();
+
+      app.use(express.static(process.cwd()));
+
+      app.listen(port, async () => {
+        const url = `http://localhost:${port}`;
+        console.log(chalk.greenBright(`Server running at ${url}`));
+        await open(url);
+      });
+    });
+};
